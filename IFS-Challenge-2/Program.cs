6c002938e86268f8993f56b2fb565123ff2a39fd
Navinda Chandrasiri <adilanga@gmail.com>
2022-09-15 19:51:57 +0530

    Add project files.

---
+++ IFS-Challenge-2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFS_Challenge_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Z = 5;
+            int Y = 6;
+            int X = Z + Y * Z - Y;
+
+            int sum = 0;
+            int count = 0;
+
+            while (count < X)
+            {
+                if (findPrime(count))
+                {
+                    sum += count;
+                }
+                count++;
+            }
+
+            Console.Write("Sum of Prime numbers below " + X + " is: " + sum);
+            Console.ReadLine();
+        }
+
+        // Find the number is Prime number
+        static bool findPrime(int num)
+        {
+            bool isPrime = true;
+
+            if (num <= 1)
+            {
+                isPrime = false;
+            }
+
+            for (int count = 2; count <= num / 2; count++)
+            {
+                if (num % count == 0)
+                {
+                    isPrime = false;
+                }
+            }
+
+            return isPrime;
+        }
+    }
+}
